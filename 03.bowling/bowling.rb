5fbc2b5443dd24e35f9568d93ef82d9b1e5b3c78
kiharakeito <kiharakeito@gmail.com>
2023-03-05 19:47:29 +0900

    ボウリングのスコア計算プログラムの修正をいたしました。
    ご指摘箇所の修正対応をいたしました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,39 +1,42 @@
 # frozen_string_literal: true
 
-# ボウリングのスコア計算
-scores = ARGV[0].split(/,/)
+scores = ARGV[0].split(',')
 
 shots = []
-shot_count = 0 # 18以上は10フレーム目
+NUMBER_IN_LASTFRAME = 19
 scores.each do |shot|
-  if shot == 'X' # strikeの場合
+  if shot == 'X'
     shots << 10
-    if shot_count < 18 # 9フレームまで実行
-      shots << 0
-      shot_count += 2
-    end
+    shots << 0 if shots.size < NUMBER_IN_LASTFRAME
   else
     shots << shot.to_i
-    shot_count += 1
   end
 end
 
-frames = []
-shots.each_slice(2) do |i|
-  frames << i
+frames = shots.each_slice(2).to_a
+
+def strike?(frame)
+  frame[0] == 10
+end
+
+def spare?(frame)
+  frame.sum == 10
 end
 
 score_total = 0
 frames.each_with_index do |frame, i|
-  if frame[0] == 10 && i < 9 # strikeかつ9フレームまでの場合
-    next_frames = frames[i + 1] # 次のフレーム取得
-    score_total += if next_frames[0] == 10 && i < 8 # 次のフレームもstrikeかつ8フレームまでの場合
-                     10 + frames[i + 2][0] # 次の次のフレームからも加算
-                   else # 次のフレームがstrikeでない場合
-                     next_frames.sum
-                   end
-  elsif frame.sum == 10 && i < 9 # spareかつ9フレームまでの場合
-    score_total += frames[i + 1][0]
+  if i < 9
+    if strike?(frame)
+      next_frame = frames[i + 1]
+      score_total +=
+        if next_frame[0] == 10 && i < 8
+          10 + frames[i + 2][0]
+        else
+          next_frame.sum
+        end
+    elsif spare?(frame)
+      score_total += frames[i + 1][0]
+    end
   end
   score_total += frame.sum
 end
