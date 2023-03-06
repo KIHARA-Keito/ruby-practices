18221c2e0015536959f633d66c84c1b7c70009c7
kiharakeito <kiharakeito@gmail.com>
2023-03-06 19:10:56 +0900

    ボウリングのスコア計算プログラムの再修正をいたしました
    ご指摘箇所の修正対応をいたしました

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,41 +1,40 @@
 # frozen_string_literal: true
 
+LAST_FRAME_NUMBER = 19
 scores = ARGV[0].split(',')
 
 shots = []
-NUMBER_IN_LASTFRAME = 19
 scores.each do |shot|
   if shot == 'X'
     shots << 10
-    shots << 0 if shots.size < NUMBER_IN_LASTFRAME
+    shots << 0 if shots.size < LAST_FRAME_NUMBER
   else
     shots << shot.to_i
   end
 end
-
-frames = shots.each_slice(2).to_a
 
 def strike?(frame)
   frame[0] == 10
 end
 
 def spare?(frame)
-  frame.sum == 10
+  !strike?(frame) && frame.sum == 10
 end
 
 score_total = 0
+frames = shots.each_slice(2).to_a
 frames.each_with_index do |frame, i|
   if i < 9
+    next_frame = frames[i + 1]
     if strike?(frame)
-      next_frame = frames[i + 1]
       score_total +=
-        if next_frame[0] == 10 && i < 8
+        if strike?(next_frame) && i < 8
           10 + frames[i + 2][0]
         else
           next_frame.sum
         end
     elsif spare?(frame)
-      score_total += frames[i + 1][0]
+      score_total += next_frame[0]
     end
   end
   score_total += frame.sum
