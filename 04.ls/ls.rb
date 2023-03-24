dd3fe9c00779e3508220842cfdf0a5012758640d
kiharakeito <kiharakeito@gmail.com>
2023-03-24 20:45:43 +0900

    ls コマンドを作る 1 の修正をしました
    メソッドなど見直して修正いたしました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -8,26 +8,23 @@ def main
   max_filename_width  = 0
   Dir.each_child('.') do |file|
     files << file unless file.match?(/^\./)
-    max_filename_width  = file.length if max_filename_width  < file.length
+    max_filename_width = file.length if max_filename_width < file.length
   end
-  two_dimensional_files = divide_into_rows(files)
-  sorted_files = flatten_nil_empty(two_dimensional_files)
-  display_files(sorted_files, max_filename_width  + 4)
+  display_files(files, max_filename_width + 4)
 end
 
-def divide_into_rows(files)
+def split_files_by_row(files)
   number_rows = (files.size.to_f / COLUMNS).ceil
   files.sort.each_slice(number_rows).to_a.map { |row| row.values_at(0..number_rows - 1) }
 end
 
-def flatten_nil_empty(files)
-  files.transpose.flatten.map { |file| file.nil? ? '' : file }
-end
-
 def display_files(files, width)
-  files.each.with_index(1) do |file, index|
-    print file.ljust(width)
-    puts if (index % COLUMNS).zero?
+  split_files_by_row(files).transpose.each do |rows|
+    rows.each do |file|
+      file = file.nil? ? '' : file
+      print file.ljust(width)
+    end
+    puts
   end
 end
 
