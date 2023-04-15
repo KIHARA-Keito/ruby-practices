4f6787da407f692af5ed5111199f0ee5e91a57df
kiharakeito <kiharakeito@gmail.com>
2023-04-15 20:41:16 +0900

    ls コマンドを作る 3 の修正を提出します
    修正いたしました。お手数ですがご確認のほどよろしくお願いいたします。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,24 +6,24 @@ require 'optparse'
 COLUMNS = 3
 
 def main
+  options = ARGV.getopts('r')
   files = []
   max_filename_width = 0
   Dir.foreach('.') do |file|
     files << file unless file.match?(/^\./)
     max_filename_width = file.length if max_filename_width < file.length
   end
-  display_files(files, max_filename_width + 4)
+  display_files(options, files, max_filename_width + 4)
 end
 
-def split_files_by_row(files)
+def split_files_by_row(options, files)
   number_rows = (files.size.to_f / COLUMNS).ceil
-  options = ARGV.getopts('r')
   files_sorted = options['r'] ? files.sort.reverse : files.sort
   files_sorted.each_slice(number_rows).to_a.map { |row| row.values_at(0..number_rows - 1) }
 end
 
-def display_files(files, width)
-  split_files_by_row(files).transpose.each do |rows|
+def display_files(options, files, width)
+  split_files_by_row(options, files).transpose.each do |rows|
     rows.each do |file|
       file = file.nil? ? '' : file
       print file.ljust(width)
