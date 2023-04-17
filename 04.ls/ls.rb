a116b0ddd427e13c6fd89a303ffaa6d695da5f18
kiharakeito <kiharakeito@gmail.com>
2023-04-18 03:07:58 +0900

    ls コマンドを作る 3 の修正を提出します
    またお手数ですがご確認のほどよろしくお願いいたします。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -13,17 +13,17 @@ def main
     files << file unless file.match?(/^\./)
     max_filename_width = file.length if max_filename_width < file.length
   end
-  display_files(options, files, max_filename_width + 4)
+  display_files(options['r'], files, max_filename_width + 4)
 end
 
-def split_files_by_row(options, files)
+def split_files_by_row(is_option_r, files)
   number_rows = (files.size.to_f / COLUMNS).ceil
-  files_sorted = options['r'] ? files.sort.reverse : files.sort
+  files_sorted = is_option_r ? files.sort.reverse : files.sort
   files_sorted.each_slice(number_rows).to_a.map { |row| row.values_at(0..number_rows - 1) }
 end
 
-def display_files(options, files, width)
-  split_files_by_row(options, files).transpose.each do |rows|
+def display_files(is_option_r, files, width)
+  split_files_by_row(is_option_r, files).transpose.each do |rows|
     rows.each do |file|
       file = file.nil? ? '' : file
       print file.ljust(width)
